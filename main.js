72ca619a873f58a382f0a18d97c3be2905a9a604
Daria Lelito <darialelito@gmail.com>
2018-12-20 20:02:31 +0100

    changes
    Add changes to nav. Create next section

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const iconBurger = document.querySelector('i.fa-bars');
 const iconX = document.querySelector('i.fa-times');
 const navContent = document.querySelector('.navigation-content');
 
+
+window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > 50) {
+        document.querySelector('.navigation').classList.add('scroll')
+    } else {
+        document.querySelector('.navigation').classList.remove('scroll');
+    }
+})
+
 clickNavBurger.addEventListener('click', function () {
     iconBurger.classList.toggle('show');
     iconX.classList.toggle('show');
